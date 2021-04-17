21552c53ad124a40ec114528c9c50b84c3ede9c9
Johannes Christ <jc@jchri.st>
2021-04-17 15:54:35 +0200

    Send error on command error.

--- ducky/error_handler.py
+++ ducky/error_handler.py
@@ -4,10 +4,9 @@ from discord.ext import commands
 
 
 class ErrorHandler(commands.Cog):
-    pass
-    # @commands.Cog.listener()
-    # async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> Any:
-    #     if isinstance(error, commands.BadArgument):
-    #         await ctx.channel.send(f"error: ``{error}``")
-    #     else:
-    #         return await ctx.bot.on_command_error(ctx, error)
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> Any:
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.channel.send("sorry, but you are not allowed to run this command")
+        else:
+            return await ctx.bot.on_command_error(ctx, error)
